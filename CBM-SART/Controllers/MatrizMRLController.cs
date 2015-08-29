5a02aaad3b147b5c69245b4378aba91786ba9c97
MarceloToapanta <marcelo.toapanta5@gmail.com>
2015-08-29 04:12:20 -0500

    Add Matriz MRL, Create View

---
+++ CBM-SART/Controllers/MatrizMRLController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CBM_SART.Models;
+using System.Linq.Dynamic;
+
+namespace CBM_SART.Controllers
+{
+    public class MatrizMRLController : Controller
+    {
+        private cbm_iso_sart_entities db = new cbm_iso_sart_entities();
+
+        // GET: /MatrizMRL/
+        public ActionResult Index(string filter = null, int page = 1, int pageSize = 15, string sort = "imr_puesto_trabajo_mrl", string sortdir = "ASC")
+        {
+            if (String.IsNullOrEmpty(filter)) { filter = null; }
+            var records = new PagedList<iso_matriz_mrl>();
+            ViewBag.filter = filter;
+            records.Content = db.iso_matriz_mrl
+                        .Where(x => filter == null ||
+                                (x.imr_puesto_trabajo_mrl.ToLower().Contains(filter.ToLower().Trim()))
+                              )
+                        .OrderBy(sort + " " + sortdir)
+                        .Skip((page - 1) * pageSize)
+                        .Take(pageSize)
+                        .ToList();
+
+            // Count
+            records.TotalRecords = db.iso_matriz_mrl
+                         .Where(x => filter == null ||
+                                (x.imr_puesto_trabajo_mrl.ToLower().Contains(filter.ToLower().Trim()))).Count();
+
+            records.CurrentPage = page;
+            records.PageSize = pageSize;
+            ViewBag.total = records.TotalRecords;
+
+            return View(records);
+
+            //return View(db.iso_empresa.ToList());
+        }
+
+        // GET: /MatrizMRL/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            iso_matriz_mrl iso_matriz_mrl = await db.iso_matriz_mrl.FindAsync(id);
+            if (iso_matriz_mrl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iso_matriz_mrl);
+        }
+
+        // GET: /MatrizMRL/Create
+        public ActionResult Create()
+        {
+            ViewBag.imr_localizacion_mrl = new SelectList(db.iso_departamento.OrderBy("ide_nombre_departamento asc"), "ide_id_departamento", "ide_nombre_departamento");
+            ViewBag.imr_cargo_mrl = new SelectList(db.iso_cargo.OrderBy("icg_nombre asc"), "icg_id_cargo", "icg_nombre");
+            return PartialView();
+        }
+
+        // POST: /MatrizMRL/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="imr_id_matriz_mrl,imr_localizacion_mrl,imr_proceso_mrl,imr_sub_proceso_mrl,imr_cargo_mrl,imr_puesto_trabajo_mrl,imr_nro_tabajadores_mrl,imr_jefe_area_mrl,imr_fecha_evaluacion_mrl,imr_descripcion_mrl,imr_id_puesto_t_mrl,imr_ubicacion")] iso_matriz_mrl iso_matriz_mrl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.iso_matriz_mrl.Add(iso_matriz_mrl);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(iso_matriz_mrl);
+        }
+
+        // GET: /MatrizMRL/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            iso_matriz_mrl iso_matriz_mrl = await db.iso_matriz_mrl.FindAsync(id);
+            if (iso_matriz_mrl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iso_matriz_mrl);
+        }
+
+        // POST: /MatrizMRL/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="imr_id_matriz_mrl,imr_localizacion_mrl,imr_proceso_mrl,imr_sub_proceso_mrl,imr_cargo_mrl,imr_puesto_trabajo_mrl,imr_nro_tabajadores_mrl,imr_jefe_area_mrl,imr_fecha_evaluacion_mrl,imr_descripcion_mrl,imr_id_puesto_t_mrl,imr_ubicacion")] iso_matriz_mrl iso_matriz_mrl)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iso_matriz_mrl).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(iso_matriz_mrl);
+        }
+
+        // GET: /MatrizMRL/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            iso_matriz_mrl iso_matriz_mrl = await db.iso_matriz_mrl.FindAsync(id);
+            if (iso_matriz_mrl == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iso_matriz_mrl);
+        }
+
+        // POST: /MatrizMRL/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            iso_matriz_mrl iso_matriz_mrl = await db.iso_matriz_mrl.FindAsync(id);
+            db.iso_matriz_mrl.Remove(iso_matriz_mrl);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult PuestosPorCargo(int id)
+        {
+            if (id != 0)
+            {
+                var iso_cargo = db.iso_cargo.Find(id);
+                var puestos = iso_cargo.iso_puesto_trabajo.Select((x => new
+                {
+                    x.ipt_id_puesto_t,
+                    x.ipt_nro_trbajadores,
+                    x.ipt_nombre_puesto_t
+                }));
+                return this.Json(puestos, JsonRequestBehavior.AllowGet);
+            }
+            return null;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- CBM-SART/Models/iso_matriz_mrl.cs
+++ CBM-SART/Models/iso_matriz_mrl.cs
@@ -11,6 +11,7 @@ namespace CBM_SART.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class iso_matriz_mrl
     {
@@ -20,14 +21,24 @@ namespace CBM_SART.Models
         }
     
         public int imr_id_matriz_mrl { get; set; }
+        [Display(Name = "Area")]
+        [Required]
         public string imr_localizacion_mrl { get; set; }
         public string imr_proceso_mrl { get; set; }
         public string imr_sub_proceso_mrl { get; set; }
+        [Display(Name = "Cargo")]
+        [Required]
         public string imr_cargo_mrl { get; set; }
+        [Display(Name = "Puesto de Trabajo")]
+        [Required]
         public string imr_puesto_trabajo_mrl { get; set; }
+        [Display(Name = "Nro. Trabajadores")]
         public Nullable<long> imr_nro_tabajadores_mrl { get; set; }
+        [Display(Name = "Jefe de Trabajo")]
         public string imr_jefe_area_mrl { get; set; }
+        [Display(Name = "Fecha de Evaluación")]
         public string imr_fecha_evaluacion_mrl { get; set; }
+        [Display(Name = "Descripción")]
         public string imr_descripcion_mrl { get; set; }
         public Nullable<long> imr_id_puesto_t_mrl { get; set; }
         public string imr_ubicacion { get; set; }
