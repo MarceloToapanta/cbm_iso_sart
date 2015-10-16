6246a7dd2a3b611075c452c3f771a6adf1431383
MarceloToapanta <marcelo.toapanta5@gmail.com>
2015-10-16 15:15:24 -0500

    Add View Rebuild login

---
+++ CBM-SART/ActionFilter/UserFilter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CBM_SART.ActionFilter
+{
+    public class UserFilter : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpContext ctx = HttpContext.Current;
+            if (ctx.Session["Usuario"] == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary {{ "Controller", "Usuario" },
+                                      { "Action", "Ingresar"}, {"url" , filterContext.HttpContext.Request.Url } });
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Log("OnActionExecuted", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Log("OnResultExecuting", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Log("OnResultExecuted", filterContext.RouteData);
+        }
+
+
+        private void Log(string methodName, RouteData routeData)
+        {
+            var controllerName = routeData.Values["controller"];
+            var actionName = routeData.Values["action"];
+            var message = String.Format("{0} controller:{1} action:{2}", methodName, controllerName, actionName);
+            Debug.WriteLine(message, "Action Filter Log");
+        }
+
+
+
+
+    }
+}
