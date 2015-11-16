391158a19dfd2c4af14ca2dff6395d44ab2ae1fd
MarceloToapanta <marcelo.toapanta5@gmail.com>
2015-11-16 04:15:28 -0500

    Permisos de Vigilancia 2

--- CBM-SART/Controllers/PermisoVigilaciaController.cs
+++ CBM-SART/Controllers/PermisoVigilaciaController.cs
@@ -16,11 +16,29 @@ namespace CBM_SART.Controllers
         {
             var userlist = new SelectList(db.iso_usuario.Where(x => x.ius_deshabilitado == "N").ToList(), "ius_id_usuario", "ius_nombre_usuario", IdUsuario);
             ViewData["Usuarios"] = userlist;
+            ViewBag.Modulos = db.iso_modulo;
             if (IdUsuario != null)
             {
                 ViewBag.Usuario = db.iso_usuario.Find(IdUsuario);
             }
             return View();
         }
-	}
+        public ActionResult Actualizar(int? IdUsuario, int[] modulo)
+        {
+            iso_usuario u = db.iso_usuario.Find(IdUsuario);
+            //Elimina los registros anteriores
+            if (u != null) {
+                u.iso_modulo.Clear();
+                db.SaveChanges();
+                //Ingresa nuevos valores
+                foreach (var nm in modulo.ToList())
+                {
+                    iso_modulo n = db.iso_modulo.Find(nm);
+                    u.iso_modulo.Add(n);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index",new{ IdUsuario = IdUsuario});
+        }
+    }
 }
